231ef4f761ed183cc1af356e6c61721b94ca1c46
Ion Utale <ion.utale@guccidigital.com>
2023-08-01 14:53:26 +0200

    added styled components and prepared a small demo on how sc works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1<{ fontSize: number, color: string
+}>`
+  font-size: ${(props) => props.fontSize}rem;
+  color: ${(props) => props.color};
+  text-align: center;
+  border: 1px solid #BF4F74;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  margin: 1rem;
+
+  &:hover {
+    color: #F7B801;
+    border-color: #F7B801;
+  }
+
+  &:active {
+    color: #3B5998;
+    border-color: #3B5998;
+  }
+
+  &:focus {
+    color: #0E2A47;
+    border-color: #0E2A47;
+  }
+`;
+
+const colors = ['#BF4F74', '#F7B801', '#3B5998', '#0E2A47', '#F7B801', '#3B5998', '#0E2A47'];
 
 export default function Home() {
+  const [fontSize, setFontSize] = useState(0.5);
+  const [color, setColor] = useState('#BF4F74');
+
+  const handleFontSize = () => {
+    setFontSize((prevFontSize) => prevFontSize + 0.1);
+  };
+
+  const handleColor = () => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <button onClick={handleFontSize}>increase font size</button>
+      <button onClick={handleColor}>change color</button>
+      <Title 
+      fontSize={fontSize}
+      color={color}
+      >Hello World</Title>
     </main>
-  )
+  );
 }
